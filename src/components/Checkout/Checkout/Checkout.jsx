a3fffd44ec5ecdcdbc66dcd9f2b10deeebba4dfe
dennis deuling <dennis.deuling@icloud.com>
2022-05-07 19:46:27 +0200

    refactored code in the checkout and implemented the stripe codes

--- src/components/Checkout/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout/Checkout.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import Commerce from '@chec/commerce.js';
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
 
 // components
 import AddressForm from './AddressForm/AddressForm';
@@ -12,12 +12,13 @@ import { useCart } from '../../../hooks/CartContext';
 // styles
 import { Wrapper } from './Checkout.styles';
 
-const commerce = new Commerce(process.env.GATSBY_CHEC_PUBLIC_KEY);
-
-function Checkout() {
+function Checkout({ ...props }) {
+	const { commerce } = props;
+	const cart = useCart();
+	const stripeElements = useElements();
+	const stripe = useStripe();
 	const [address, setAddress] = useState(null);
 	const [isChecked, setIsChecked] = useState(false);
-	const cart = useCart();
 	const [checkoutData, setCheckoutData] = useState(null);
 
 	useEffect(() => {
@@ -47,6 +48,52 @@ function Checkout() {
 		setIsChecked(isChecked);
 	};
 
+	// Create a function that can be called when a "complete order" button is clicked
+	async function captureOrder() {
+		const orderDetails = '';
+		// This process includes a few API calls, so now is a good time to show a loading indicator
+
+		// Create a payment method using the card element on the page
+		const paymentMethodResponse = await stripe.createPaymentMethod({
+			type: 'card',
+			card: stripeElements.getElement(CardElement)
+		});
+
+		if (paymentMethodResponse.error) {
+			// There was some issue with the information that the customer entered into the payment details form.
+			alert(paymentMethodResponse.error.message);
+			return;
+		}
+
+		try {
+			// Use a checkout token ID generated that was generated earlier, and any order details that may have been collected
+			// on this page. Note that Commerce.js checkout tokens may already have all the information saved against them to
+			// capture an order, so this extra detail may be optional.
+			const order = await commerce.checkout.capture(checkoutData.id, {
+				...orderDetails,
+				// Include Stripe payment method ID:
+				payment: {
+					gateway: 'stripe',
+					stripe: {
+						payment_method_id: paymentMethodResponse.paymentMethod.id
+					}
+				}
+			});
+
+			// If we get here, the order has been successfully captured and the order detail is part of the `order` variable
+			console.log(order);
+			return;
+		} catch (response) {
+			// There was an issue with capturing the order with Commerce.js
+			console.log(response);
+			alert(response.message);
+			return;
+		} finally {
+			// Any loading state can be removed here.
+		}
+	}
+
+	console.log(checkoutData);
 	return (
 		<Wrapper>
 			<div>
@@ -72,6 +119,10 @@ function Checkout() {
 				<fieldset>
 					<legend>Your Shipping country:</legend>
 					<SelectField methods={checkoutData?.shipping_methods} />
+				</fieldset>
+				<fieldset>
+					<legend>Your Payment:</legend>
+					<CardElement />
 				</fieldset>
 			</div>
 		</Wrapper>
--- src/pages/checkout/checkout.jsx
+++ src/pages/checkout/checkout.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 
 // components
 import Checkout from '../../components/Checkout/Checkout/Checkout';
 
 // context
 import CartProvider from '../../hooks/CartContext';
+import Commerce from '@chec/commerce.js';
+const StripeContext = ({ children }) => <Elements stripe={stripe}>{children}</Elements>;
+
+const commerce = new Commerce(process.env.GATSBY_CHEC_PUBLIC_KEY);
+const stripe = loadStripe(process.env.GATSBY_STRIPE_PUBLIC_KEY);
 
 function CheckoutPage() {
 	return (
 		<CartProvider>
-			<Checkout />
+			<StripeContext>
+				<Checkout commerce={commerce} />
+			</StripeContext>
 		</CartProvider>
 	);
 }
